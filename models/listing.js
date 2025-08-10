9232cb45d55324b0c4c51f067d2ec15cf02d0cea
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-10 23:53:05 +0530

    Handling Delete Listing

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -33,6 +34,12 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = model("Listing", listingSchema);
