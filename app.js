cd01c0d25ed4131451c342dc65aa640404273bc6
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-10 23:32:06 +0530

    Delete Review Route

--- app.js
+++ app.js
@@ -147,6 +147,17 @@ app.post(
   })
 );
 
+// Delete Review Route
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  })
+);
+
 //Testing listing model
 // app.get("/testlisting", async (req, res) => {
 //   let listing1 = new Listing({
