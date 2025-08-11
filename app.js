37874827ea42e74788cd293e64aa79d43bbc4131
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-11 10:10:34 +0530

    Restructuring Reviews with Express Router

--- app.js
+++ app.js
@@ -5,27 +5,18 @@ const mongoose = require("mongoose");
 const port = 8080;
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
 
 const listings = require("./routes/listing.js");
-
-const Listing = require("./models/listing.js");
-const Review = require("./models/review.js");
+const reviews = require("./routes/review.js");
 
 app.use(methodOverride("_method"));
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-
 app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/js")));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -43,62 +34,10 @@ app.get("/", (req, res) => {
   res.send("working");
 });
 
-// Validate Review Schema
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
 // Listing
 app.use("/listings", listings);
-
-// New Review
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-// Delete Review Route
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-//Testing listing model
-// app.get("/testlisting", async (req, res) => {
-//   let listing1 = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Goa",
-//     country: "India",
-//   });
-//   await listing1.save();
-//   console.log("sample saved");
-//   res.send("Sucessful");
-// });
+// Review
+app.use("/listings/:id/reviews", reviews);
 
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
@@ -106,7 +45,6 @@ app.all(/.*/, (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  // res.status(statusCode).send(message);
   res.status(statusCode).render("error.ejs", { message });
 });
 
