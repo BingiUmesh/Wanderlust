0f7c71c332f22d1467c515da5ab3baf5ded943c5
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-12 09:47:32 +0530

    Styling

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ passport.deserializeUser(User.deserializeUser()); //deserialize users into sessi
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
   next();
 });
 
