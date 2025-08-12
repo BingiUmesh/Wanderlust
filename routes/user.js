e32091ca95a28a02290aed1fec6482bb9454b338
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-12 10:44:11 +0530

    Post Login page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signUp.ejs");
@@ -40,13 +41,16 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    console.log(res.locals.redirectUrl);
+    res.redirect(redirectUrl);
   }
 );
 
