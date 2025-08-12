bfee41754ddbbbb09ad8abe42fc5a2a4a1f1d4f9
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-12 13:22:35 +0530

    Hide Edit & Delete for unauthorized users

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    console.log(req.originalUrl);
     req.session.redirectUrl = req.originalUrl;
     req.flash("error", "You must be logged in to create listing!");
     return res.redirect("/login");
