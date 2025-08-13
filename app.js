85b39d5135ffbbdcc9c08e278a145e5136d69e9d
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-13 22:30:06 +0530

    Mongo Session

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo"); //mongo store
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -30,6 +31,8 @@ app.use(express.static(path.join(__dirname, "public/js")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const dbUrl = process.env.ATLASDB_URL;
+
 main()
   .then((res) => {
     console.log("DB Connected Sucessfully");
@@ -37,10 +40,24 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  // await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(dbUrl);
 }
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: "mysupersecretcode",
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
+  store: store,
   secret: "mysupersecretcode",
   resave: false,
   saveUninitialized: true,
