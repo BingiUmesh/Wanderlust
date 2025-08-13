81edd6a8b703a9ebb09266fac46659fa96a37e9f
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-13 16:02:08 +0530

    Removing search from Login & SignUp

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currentUser = req.user;
+  res.locals.originalUrl = req.originalUrl;
   next();
 });
 
